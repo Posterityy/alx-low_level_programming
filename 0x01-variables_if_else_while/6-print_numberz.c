d16bee13f61757a2b613a17bb457d387ce684dbe
posterityy <ebojjonathan@gmail.com>
2022-09-11 02:19:26 -0700

    all-2

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/**	 
+/**
  * main - entry point
  *
  * Return: return 0
- */ 
+ */
 int main(void)
 {
 	int a = 0;
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 * main - entry point
 *
 * Return: return 0
-*/ 
+*/
 int main(void)
 {
 	char a;
