927f60d7393aaeaaba25e1b6d4820b67bb8a6ed8
posterityy <ebojjonathan@gmail.com>
2022-09-11 01:36:55 -0700

    102-1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,22 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; 1 <= 99; i++)
+	for (i = 0; 1 < 100; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-
-			if (!(i == 98 && j == 99))
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
