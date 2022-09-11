7a697902075d5d08b5dee8cdfecc1169ebd1ce14
posterityy <ebojjonathan@gmail.com>
2022-09-11 01:29:37 -0700

    101 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,26 +3,23 @@
  * main - entry point
  *
  * Return: always return 0
- *
  */
 int main(void)
 {
-	int i;
-	int j;
-	int k;
+	int n, m, l;
 
-	for (i = 0; 1 <= 9; i++)
+	for (n = 48; n < 58; n++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (m = 49; m < 58; m++)
 		{
-			for (k = 0; k <= 9; k++)
+			for (l = 50; l < 58; l++)
 			{
-				if (k > j && j > i)
+				if (l > m && m > n)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i != 7 || j != 8 || k != 9)
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
